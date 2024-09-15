d8ffb1b45baea3e2e39d23f5db362444d839c9f6
dngvbngc <dvbngoc.0706@gmail.com>
2024-09-15 17:53:40 +0800

    Edit home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,55 @@
-import background from "@/assets/homepage-bg.png";
+"use client";
+
+import banner from "@/assets/homepage-banner.svg";
+import { Box, Center, Heading, Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
+import useAuthStore from "./lib/store";
+import Link from "next/link";
 
 export default function Home() {
+  const { user } = useAuthStore();
+
   return (
-    <Image
-      className='background-img'
-      src={background}
-      alt='background'
-      style={{
-        position: "absolute",
-        top: 0,
-        zIndex: -1,
-        objectFit: "cover",
-        width: "100dvw",
-        height: "100dvh",
-      }}
-    ></Image>
+    <Center padding={10}>
+      <VStack>
+        <Box textAlign='center'>
+          <Heading fontSize='6xl' padding={1}>
+            You are not{" "}
+            <Text as='span' color='#c99276'>
+              toasted
+            </Text>
+          </Heading>
+          <Box fontFamily='Comic Sans MS' fontSize='large'>
+            <Text>your on-the-go, simple and efficient notes</Text>
+            {user ? (
+              <Text>
+                view your toasts in{" "}
+                <Link href={`/${user}`}>
+                  <strong>dashboard</strong>
+                </Link>{" "}
+              </Text>
+            ) : (
+              <Text>
+                <Link href='/login'>
+                  <strong>log in</strong>
+                </Link>{" "}
+                or{" "}
+                <Link href='/signup'>
+                  <strong>sign up</strong>
+                </Link>{" "}
+                to start memorizing
+              </Text>
+            )}
+          </Box>
+        </Box>
+        <Box>
+          <Image
+            src={banner}
+            alt='banner'
+            style={{ padding: "1vh 2vw", width: "80vw" }}
+          ></Image>
+        </Box>
+      </VStack>
+    </Center>
   );
 }
